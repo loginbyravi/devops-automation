b2853d3092baffc786fce69c6781936f1a5bc6b8
Ravi <loginbyravi@gmail.com>
2023-10-11 04:06:41 +0530

    adding jenkins basic file

--- src/main/java/com/rkant/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/rkant/devopsintegration/DevopsIntegrationApplication.java
@@ -1,5 +1,7 @@
 package com.rkant.devopsintegration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DevopsIntegrationApplication {
+public class DevopsIntegrationApplication  {
 
+	
+	public static final Logger logger = LoggerFactory.getLogger(DevopsIntegrationApplication.class);
 	
 	@GetMapping("/")
 	public String getMessage() { return "Welcome to rkant world!"; }
